b959114d2b6fdfe1c4157a0a49543feae5d71f79
ursuadina <ursuadina98@yahoo.com>
2018-12-19 20:06:23 +0200

    Image thumbnail

--- proiectDaw/Controllers/PictureController.cs
+++ proiectDaw/Controllers/PictureController.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using proiectDaw.Models;
+using System;
 using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -11,7 +14,43 @@ namespace proiectDaw.Controllers
         // GET: Picture
         public ActionResult Index()
         {
-            return View();
+            List<Pictures> images = new List<Pictures> {
+                new Pictures{Id = 1,
+                            Name = "Poza 1",
+                            Image = System.IO.File.ReadAllBytes(@"E:\Google\AndroidFundamentals\app\src\main\res\drawable\poster_sparkling.jpg"),
+                            CategoryId = 1 },
+                new Pictures{Id = 2,
+                            Name = "Poza 2",
+                            Image = System.IO.File.ReadAllBytes(@"E:\Google\AndroidFundamentals\app\src\main\res\drawable\poster_sparkling.jpg"),
+                            CategoryId = 1 } };
+            return View(images);
         }
+
+        [HttpPost]
+        public ActionResult Index(int imageId)
+        {
+            List<Pictures> images = new List<Pictures> {
+                new Pictures{Id = 1,
+                            Name = "Poza 1",
+                            Image = System.IO.File.ReadAllBytes(@"E:\Google\AndroidFundamentals\app\src\main\res\drawable\poster_sparkling.jpg"),
+                            CategoryId = 1 },
+                new Pictures{Id = 2,
+                            Name = "Poza 2",
+                            Image = System.IO.File.ReadAllBytes(@"E:\Google\AndroidFundamentals\app\src\main\res\drawable\poster_sparkling.jpg"),
+                            CategoryId = 1 } };
+            /* Pictures image = images.Find(p => p.Id == imageId);
+             if (image != null)
+             {
+                 ViewBag.Base64String = "data:image/png;base64," + Convert.ToBase64String(image.Image, 0, image.Image.Length);
+             }*/
+            //ViewBag.Base64String = "data:image/png;base64," + Convert.ToBase64String(images.Image, 0, image.Image.Length);
+
+            return View(images);
+        }
+
+       /*ivate List<Pictures> GetImages()
+        {
+            string query = "SELECT * FROM 
+        }*/
     }
 }
--- proiectDaw/Controllers/ProfileController.cs
+++ proiectDaw/Controllers/ProfileController.cs
@@ -1,17 +1,25 @@
-﻿using System;
+﻿using Microsoft.AspNet.Identity;
+using proiectDaw.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Microsoft.AspNet.Identity.Owin;
+using Microsoft.Owin.Security;
 
 namespace proiectDaw.Controllers
 {
     public class ProfileController : Controller
     {
         // GET: Profile
+        private ApplicationDbContext db = ApplicationDbContext.Create();
+
         public ActionResult Index()
         {
-            return View();
+            String userid = User.Identity.GetUserId();
+            var user = db.Users.Find(userid);
+            return View(user);
         }
     }
 }
